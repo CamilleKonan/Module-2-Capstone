1d0f1d34294bfb460247855dce92348d3ebd8fa1
Matthew Butterfield <mbutterfield322@gmail.com>
2024-08-20 15:56:54 -0700

    capstone update models

--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -7,4 +7,28 @@ public class Account {
     private int userId;
     private BigDecimal balance;
         // Getters and Setters
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
