74201eb44af224bca2df33c306b2e04767dce06f
Eric <ebembridge54@gmail.com>
2024-08-22 19:58:11 -0400

    Capstone 2 EB

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,4 +1,14 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.model.Account;
+
+import java.math.BigDecimal;
+
 public interface AccountDao {
+
+    BigDecimal getBalance(int userId);
+
+    Account getUserById(int userId);
+
+    boolean updateBalance(int accountId, BigDecimal newBalance);
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,4 +1,49 @@
 package com.techelevator.tenmo.dao;
 
-public class JdbcAccountDao implements AccountDao{
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class JdbcAccountDao implements AccountDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public BigDecimal getBalance(int userId) {
+        String sql = "SELECT balance FROM account WHERE user_id = ?";
+        return jdbcTemplate.queryForObject(sql, BigDecimal.class, userId);
+    }
+
+    @Override
+    public getByUserId(int userId) {
+        String sql = "SELECT account_id, user_id, balance FROM account WHERE user_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
+        if (results.next()) {
+            return mapRowToAccount(results);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean updateBalance(int accountId, BigDecimal newBalance) {
+        String sql = "UPDATE account SET balance = ? WHERE account_id = ?";
+        int rowsAffected = jdbcTemplate.update(sql, newBalance, accountId);
+        return rowsAffected > 0;
+    }
+
+    private com.techelevator.tenmo.server.model.Account mapRowToAccount(SqlRowSet rs) {
+        com.techelevator.tenmo.server.model.Account account = new com.techelevator.tenmo.server.model.Account();
+        account.setAccountId(rs.getInt("account_id"));
+        account.setUserId(rs.getInt("user_id"));
+        account.setBalance(rs.getBigDecimal("balance"));
+        return account;
+    }
 }
+
