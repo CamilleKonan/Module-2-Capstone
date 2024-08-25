76b802e2c25a80ee2856a7b9816e7f96b2721263
Eric <ebembridge54@gmail.com>
2024-08-25 13:46:41 -0400

    Capstone Project 2 EB3

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,9 +1,11 @@
 package com.techelevator.tenmo.controller;
 
+import com.techelevator.tenmo.dao.JdbcAccountDao;
+import com.techelevator.tenmo.dao.JdbcUserDao;
 import com.techelevator.tenmo.model.Account;
-import com.techelevator.tenmo.services.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,25 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.security.Principal;
 
+
+@PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping("/account")
 public class AccountController {
-    private final AccountService accountService;
-    public AccountController(AccountService accountService) {
-        this.accountService = accountService;
+    private final JdbcAccountDao jdbcAccountDao;
+    private final JdbcUserDao jdbcUserDao;
+
+    public AccountController(JdbcAccountDao jdbcAccountDao, JdbcUserDao jdbcUserDao) {
+        this.jdbcAccountDao = jdbcAccountDao;
+        this.jdbcUserDao = jdbcUserDao;
     }
 
     @GetMapping("/balance")
     public ResponseEntity<BigDecimal> getBalance(Principal principal) {
         String username = principal.getName();
-        BigDecimal balance = accountService.getBalance(username);
+        int userId = jdbcUserDao.getUserByUsername(username).getId();
+        BigDecimal balance = jdbcAccountDao.getBalance(userId);
         return new ResponseEntity<>(balance, HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<Account> getAccount(Principal principal) {
         String username = principal.getName();
-        Account account = accountService.getAccountByUsername(username);
+        int userId = jdbcUserDao.getUserByUsername(username).getId();
+        Account account = jdbcAccountDao.getAccountById(userId);
         if (account != null) {
             return new ResponseEntity<>(account, HttpStatus.OK);
         } else {
--- tenmo-server/src/main/java/com/techelevator/tenmo/model/User.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/User.java
@@ -99,4 +99,5 @@ public class User {
               ", authorities=" + authorities +
               '}';
    }
+
 }
